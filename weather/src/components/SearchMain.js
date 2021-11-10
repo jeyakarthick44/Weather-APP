4248051f4cb5bc25c43245797f3d929b05bda664
jeyakarthick44 <jeyakarthick408@gmail.com>
2021-11-10 13:20:18 +0530

    Completed , Cool as a Cucumber

--- weather/src/components/SearchMain.js
+++ weather/src/components/SearchMain.js
@@ -1,20 +1,66 @@
-import React, {useState} from 'react'
+import React, { useEffect, useState } from "react";
 import "../components/style.css";
-import  WeatherDetails from "./WeatherDetails"
+import WeatherDetails from "./WeatherDetails";
 
- function SearchMain() { 
-     const [searchTerm,setSearchTerm]=useState("")
-    return (
-        <>
-        <div className="wrap">
-            <div className="search">
-                <input onChange={(e) => {setSearchTerm(e.target.value)}} value={searchTerm}type="search" placeholder="Type City Name" id="search"/>
-            </div>   
-               <button className="searchButton" > Search</button>
+const ApiId = "f2c9c7b28f2b9f4d1dd98ad1591ee56c";
+
+const SearchMain = () => {
+  const [searchTerm, setSearchTerm] = useState("madurai");
+  const [tempInfo, setTempInfo] = useState({});
+
+  const getWeatherInfo = async () => {
+    try {
+      let url = `http://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&units=metric&appid=${ApiId}`;
+
+      let res = await fetch(url);
+      let data = await res.json();
+      console.log(data)
+      const { temp, humidity, pressure } = data.main;
+      const { main: weatherType } = data.weather[0];
+      const { name } = data;
+      const { speed } = data.wind;
+      const { country, sunset } = data.sys;
+
+      const myNewWeatherInfo = {
+        temp,
+        humidity,
+        pressure,
+        weatherType,
+        name,
+        speed,
+        country,
+        sunset,
+      };
+
+      setTempInfo(myNewWeatherInfo);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getWeatherInfo();
+  }, []);
+
+  return (
+    <>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="search"
+            placeholder="Search city.."
+            id="search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button className="searchButton" onClick={getWeatherInfo}>
+            Search
+          </button>
         </div>
-        <WeatherDetails />
-        </>
-    )
+      </div>
+      <WeatherDetails {...tempInfo} />
+    </>
+  );
 }
 
 export default SearchMain;
--- weather/src/components/WeatherDetails.js
+++ weather/src/components/WeatherDetails.js
@@ -1,19 +1,57 @@
-import React from 'react';
+import React,{useState,useEffect}from 'react';
 
-function WeatherDetails() {
+function WeatherDetails({
+    temp,
+    humidity,
+    pressure,
+    weatherType,
+    name,
+    speed,
+    country,
+    sunset
+}
+) {
+    const [weatherState,setWeatherState] = useState("")
+
+    useEffect(() => {
+       if(weatherType){
+           switch(weatherType){
+                case "Clouds":
+                   setWeatherState("wi-day-cloudy")
+                   break;
+                case "Haze":
+                    setWeatherState("wi-fog")
+                    break;
+                case "Clear":
+                    setWeatherState("wi-day-sunny")
+                    break;
+                case "Mist":
+                    setWeatherState("wi-dust")
+                    break;
+           }
+       }
+    }, [weatherType])
+
+
+
+
+
+    let sec = sunset;
+    let date = new Date(sec * 1000);
+    let timeStr = `${date.getHours()}:${date.getMinutes()}`;
     return (
         <>
             <article class="widget">
                 <div className="weatherIcon">
-                    <i className="wi wi-day-sunny"></i>
+                    <i className={`wi ${weatherState}`}></i>
                 </div>
                 <div className="weatherInfo">
                     <div className="temperature">
-                        <span>23.99&deg;</span>
+                        <span>{temp}</span>
                     </div>
                     <div className="description">
-                        <div className="weatherCondition">sunny</div>
-                        <div className="place">mumbai</div>
+                        <div className="weatherCondition">{weatherType}</div>
+                        <div className="place">{name},{country}</div>
                     </div>
                 </div>
                 <div className="date">{new Date().toLocaleString()}</div>
@@ -23,13 +61,13 @@ function WeatherDetails() {
                             <p>
                                 <i className="wi wi-sunset"></i>
                             </p>
-                            <p className="extra-info-left-side">6.30 <br /> sunset</p>
+                            <p className="extra-info-left-side">{timeStr} PM<br /> sunset</p>
                         </div>
                         <div className="two-sided-section">
                             <p>
                                 <i className="wi wi-humidity"></i>
                             </p>
-                            <p className="extra-info-left-side">324 <br /> humidity</p>
+                            <p className="extra-info-left-side">{humidity}<br /> humidity</p>
                         </div>
                         </div>
                         <div className="weather-extra-info">
@@ -37,13 +75,13 @@ function WeatherDetails() {
                             <p>
                                 <i className="wi wi-rain"></i>
                             </p>
-                            <p className="extra-info-left-side">5%&* <br /> rain</p>
+                            <p className="extra-info-left-side">{pressure} <br /> rain</p>
                         </div>
                         <div className="two-sided-section">
                             <p>
                                 <i className="wi wi-strong-wind"></i>
                             </p>
-                            <p className="extra-info-left-side">324speed <br /> wind</p>
+                            <p className="extra-info-left-side">{speed} <br /> wind</p>
                         </div>
                     </div>
                 </div>
